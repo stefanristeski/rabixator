60bac6777f085d3036cbde1fca45dd6b46210bac
stefanristeski <stefan.risteski@sbgenomics.com>
2015-06-23 11:33:05 +0200

    some naming changed

--- cwl_parser_v2.py
+++ cwl_parser_v2.py
@@ -1,17 +1,19 @@
 """Rabix Parser
 
 Usage:
-  cwl_parser.py --file=<file> [-b --int=<integer> --float=<float> -s=<string> --int-array=<int>... --enum=<string>]
+  cwl_parser.py --file=<file> [-b --longboolean --some-int INTEGER --some-float=<float> -s=<string> --some-array=<int>... --enum=<string>]
+  cwl_parser.py -h | -v
 
 Options:
   -h --help                         show this help message and exit
   -v, --version                     show version and exit
   --file=<file>                     this is file
   -s --string=<string>              this is string
-  --int=<integer>                   this is int
-  --float=<float>                   this is float [default: 10.0] #DETAILED DESCRIPTION
+  -i, --some-int INTEGER            this is int
+  --some-float=<float>              this is float [default: 10.0] #DETAILED DESCRIPTION
   -b                                this is boolean
-  --int-array=<int>...              this is list of int [default: 1 2 3] #ARRAY
+  --longboolean                     this is long boolean
+  --some-array=<int>...             this is list of int [default: 1 2 3] #ARRAY
   --enum=<enum>                     this is enum [values: x y c]
 
 Try:
@@ -48,23 +50,22 @@ if __name__ == '__main__':
 
     # Append to rabix input
     def append_input(prefix, var_type):
-        description = ''
         label = ''
+        description = ''
 
         for line in options:
             if prefix in line:
-                label = line.split('  ')[-1].strip()
-
-            if prefix in line and '#' in line:
-                description = line.rsplit('#')[-1]
-                label = line.rsplit('#')[0].split('  ')[-1].strip()
+                label = prefix.lower().strip('-').replace('-', '_')
+                description = line.split('  ')[-1].strip()
+                if var_type == 'boolean' and not prefix.startswith('--'):
+                    label = description
 
         inputs = rabix_schema.get('inputs')
         inputs.append(OrderedDict(
             {
                 "inputBinding": {"prefix": prefix, "separate": True},
                 "type": var_type if 'enum' in str(var_type) or 'array' in str(var_type) else ["null", var_type],
-                "id": ''.join(['#', prefix.lower().strip('-')]),
+                "id": ''.join(['#', prefix.lower().strip('-').replace('-', '_')]),
                 "depth": 0,
                 "description": description,
                 "label": label,
@@ -82,34 +83,37 @@ if __name__ == '__main__':
         pass
 
     # Iterate over args and options, and call append_input with proper arguments
-    for key, value in args.iteritems():
+    for prefix, value in args.iteritems():
         if isinstance(value, bool):
-            append_input(key, 'boolean')
-            print 'boolean'
+            if prefix in ['--version', '--help']:
+                continue
+            else:
+                append_input(prefix, 'boolean')
+                print 'boolean'
         elif isinstance(value, list):
             for line in options:
-                if key in line and map_any(line, str_type):
-                    append_input(key, ["null", {"type": "array", "items": {"type": "string"}}])
-                elif key in line and map_any(line, int_type):
-                    append_input(key, ["null", {"type": "array", "items": {"type": "int"}}])
-                elif key in line and map_any(line, float_type):
-                    append_input(key, ["null", {"type": "array", "items": {"type": "float"}}])
-                elif key in line and map_any(line, file_type):
-                    append_input(key, ["null", {"type": "array", "items": {"type": "File"}}])
+                if prefix in line and map_any(line, str_type):
+                    append_input(prefix, ["null", {"type": "array", "items": {"type": "string"}}])
+                elif prefix in line and map_any(line, int_type):
+                    append_input(prefix, ["null", {"type": "array", "items": {"type": "int"}}])
+                elif prefix in line and map_any(line, float_type):
+                    append_input(prefix, ["null", {"type": "array", "items": {"type": "float"}}])
+                elif prefix in line and map_any(line, file_type):
+                    append_input(prefix, ["null", {"type": "array", "items": {"type": "File"}}])
         else:
             for line in options:
-                if key in line and map_any(line, str_type) and map_all(line, str_type):
-                    append_input(key, 'string')
-                elif key in line and map_any(line, int_type) and map_all(line, int_type):
-                    append_input(key, 'int')
-                elif key in line and map_any(line, float_type) and map_all(line, float_type):
-                    append_input(key, 'float')
-                elif key in line and map_any(line, file_type) and map_all(line, file_type):
-                    append_input(key, 'File')
-                elif key in line and map_any(line, enum_type):
-                    value = ["null", {"type": "enum", "name": key.lower().strip('-'),
+                if prefix in line and map_any(line, str_type) and map_all(line, str_type):
+                    append_input(prefix, 'string')
+                elif prefix in line and map_any(line, int_type) and map_all(line, int_type):
+                    append_input(prefix, 'int')
+                elif prefix in line and map_any(line, float_type) and map_all(line, float_type):
+                    append_input(prefix, 'float')
+                elif prefix in line and map_any(line, file_type) and map_all(line, file_type):
+                    append_input(prefix, 'File')
+                elif prefix in line and map_any(line, enum_type):
+                    value = ["null", {"type": "enum", "name": prefix.lower().strip('-'),
                                       "symbols": [v for v in line.rsplit('values: ')[-1].strip(']').split(' ')]}]
-                    append_input(key, value)
+                    append_input(prefix, value)
 
     # Write rabix schema to output.json
     with open('output.json', 'w') as out_file:
